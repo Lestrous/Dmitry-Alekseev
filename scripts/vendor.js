fada73299e54023f8c23f42b106c7fabb05c827d
Leonid <shtol.leonid@mail.ru>
2019-09-24 17:38:01 +0500

    initial commit

---
+++ scripts/vendor.js
@@ -0,0 +1,29 @@
+$(function() {
+	var windowTop = self.pageYOffset || (document.documentElement && document.documentElement.scrollTop) || (document.body && document.body.scrollTop);	// высота страницы
+	var viewportWidth = innerWidth;	// ширина страницы
+
+	$('.carousel').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		variableWidth: true,
+		responsive: [
+			{
+				breakpoint: 1025,
+				settings: {
+					slidesToShow: 2,
+					dots: true,
+					arrows: false
+				}
+			},
+			{
+				breakpoint: 621,
+				settings: {
+					slidesToShow: 1,
+					dots: true,
+					arrows: false
+				}
+			}
+		]
+  });
+});
